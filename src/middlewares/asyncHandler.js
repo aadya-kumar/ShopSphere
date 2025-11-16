0174d64baaed968ca5fd62980e01d2373706fd3d
aadya-kumar <117282728+aadya-kumar@users.noreply.github.com>
2025-11-17 02:55:26 +0530

    Delete src directory

--- src/middlewares/asyncHandler.js
+++
@@ -1,6 +0,0 @@
-// backend/src/middlewares/asyncHandler.js
-// wrapper to catch async errors and forward to errorHandler
-module.exports = fn => (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
-  
